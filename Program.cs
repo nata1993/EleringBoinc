c1281340555fc0ba21c41098821ee206f5d8aa9a
kebo <bogdan.parubok@hotmail.com>
2020-03-21 15:13:39 +0200

    v1.0.2 - added controlling method for user inputed data

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System;
 using System.Text;
 using System.Threading;
 
+//user provided price can be inserted with coma or dot
 //save user provided highest electricity price and use it for next program startup
 //timestamp that follows o'clock timing no matter fro what time program was started
 //calculate spent electricity and its cost
@@ -76,105 +77,117 @@ namespace BoincElectricity
             OutputEncoding = Encoding.UTF8; //make console show utf-8 characters
 
             int retryCounter = 1;   //counter for elering data aquisition
-            byte userSpecifiedElectricityPrice; //user provided maximum electricity price he/she wants to run boinc at
+            string userSpecifiedElectricityPrice; //user provided maximum electricity price he/she wants to run boinc at
+            decimal numericalSpecifiedElectricityPrice;
+
             CallElering elering = new CallElering();    //create object for elering time and price data
             Process boinc = new Process();  //create process of external program to be run
             boinc.StartInfo.UseShellExecute = false;    //start only executables e.g.: .exe
             boinc.StartInfo.FileName = "C:\\Program Files\\BOINC program\\boincmgr";    //program file path
-
-            Write("Please provide highest electricity price you want to run\nprogram in megawats per hour pricing (e.g 45 as in 45€/MWh): ");
-            userSpecifiedElectricityPrice = byte.Parse(ReadLine());
 
             //MAIN PROCESS LOOP
 
             //loop for electricity price check and program start up and shut down
             while (true)
             {
+                //Ask user for electricity price and convert it to numerical representation
                 try
                 {
-                    //ASK FOR ELECTRICITY DATA
-                    Clear();
-                    WriteLine($"{retryCounter}) Getting data from Elering");
-                    WriteLine("=========================\n");
-                    elering.PublicGetApiData();   //getting data from elering
+                    Write("Please provide highest electricity price you want to run\nprogram in megawats per hour pricing (e.g 45 as in 45€/MWh): ");
+                    userSpecifiedElectricityPrice = ReadLine();
+                    numericalSpecifiedElectricityPrice = decimal.Parse(userSpecifiedElectricityPrice);
 
                     try
                     {
-                        //CHECK FOR ALL RUNNING PROCESSES
-                        PriceText(elering.Time, elering.Price);     //Show aqcuired data from Elering
-                        Process[] processList = Process.GetProcessesByName("BOINC");   //Search for running processes by name
-                        string allRunningProcesses = processList[0].ToString();   //convert acquired process into string for later use
+                        //ASK FOR ELECTRICITY DATA
+                        Clear();
+                        WriteLine($"{retryCounter}) Getting data from Elering");
+                        WriteLine("=========================\n");
+                        elering.PublicGetApiData();   //getting data from elering
 
-                        //CHECK FOR RUNNING EXTERNAL PROCESS
-                        if (elering.Price < userSpecifiedElectricityPrice && allRunningProcesses is "System.Diagnostics.Process (boinc)")    //if process running and price is below specified, continue running boinc
+                        try
                         {
-                            WriteLine("Electricity price is still good!");
-                            WriteLine("Boinc will continue crunching numbers.");
-                            //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
+                            //CHECK FOR ALL RUNNING PROCESSES
+                            PriceText(elering.Time, elering.Price);     //Show aqcuired data from Elering
+                            Process[] processList = Process.GetProcessesByName("BOINC");   //Search for running processes by name
+                            string allRunningProcesses = processList[0].ToString();   //convert acquired process into string for later use
 
-                            Thread.Sleep(10000);
-                        }
-                        else
-                        {
-                            WriteLine("Price is too high for cheap number crunching!");
-                            WriteLine("Shutting down Boinc!");
-                            WriteLine("Will check price again at next o'clock.");
-
-                            try
+                            //CHECK FOR RUNNING EXTERNAL PROCESS
+                            if (elering.Price < numericalSpecifiedElectricityPrice && allRunningProcesses is "System.Diagnostics.Process (boinc)")    //if process running and price is below specified, continue running boinc
                             {
-                                foreach (Process proc in Process.GetProcessesByName("BOINC"))
+                                WriteLine("Electricity price is still good!");
+                                WriteLine("Boinc will continue crunching numbers.");
+                                //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
+
+                                Thread.Sleep(10000);
+                            }
+                            else
+                            {
+                                WriteLine("Price is too high for cheap number crunching!");
+                                WriteLine("Shutting down Boinc!");
+                                WriteLine("Will check price again at next o'clock.");
+
+                                try
                                 {
-                                    proc.Kill();
+                                    foreach (Process proc in Process.GetProcessesByName("BOINC"))
+                                    {
+                                        proc.Kill();
+                                    }
                                 }
-                            }
-                            catch (Exception e)
-                            {
-                                Clear();
-                                WriteLine(e);
-                            }
-                            boinc.Kill();   //not very elegant way for stopping process...... NOT WORKING!
-                            boinc.WaitForExit();
-                            //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
+                                catch (Exception e)
+                                {
+                                    Clear();
+                                    WriteLine(e);
+                                }
+                                boinc.Kill();   //not very elegant way for stopping process...... NOT WORKING!
+                                boinc.WaitForExit();
+                                //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
 
-                            Thread.Sleep(10000);
+                                Thread.Sleep(10000);
+                            }
+                        }
+                        //EXTERNAL PROCESS IS NOT RUNNING
+                        catch (IndexOutOfRangeException)    //if there is no process running, this exception is thrown and process is started
+                        {
+                            PriceText(elering.Time, elering.Price);     //Show aqcuired data from Elering
+                            WriteLine("Boinc is not crunching numbers.\n");
+
+                            //START EXTERNAL PROCESS
+                            if (elering.Price < numericalSpecifiedElectricityPrice)   //if price is below 45€/MWh and got data from elering, proceed to start process
+                            {
+                                boinc.Start();  //start process based on previous setup
+                                Thread.Sleep(13000);    //wait 13 seconds for program to connect to internet and get data from internet
+                                boinc.CloseMainWindow();    //close program window automatically
+
+                                WriteLine($"BOINC started crunching numbers!");
+                                WriteLine("Will check price again at next o'clock.\n");
+                                //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
+
+                                Thread.Sleep(10000);
+                            }
+                            //WAIT FOR SUITABLE MOMENT TO START PROCESS
+                            else
+                            {
+                                WriteLine($"Price is still too high for cruncing numbers!");
+                                WriteLine("Will check price again at next o'clock.\n");
+                                //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
+
+                                Thread.Sleep(10000);
+                            }
                         }
                     }
-                    //EXTERNAL PROCESS IS NOT RUNNING
-                    catch (IndexOutOfRangeException)    //if there is no process running, this exception is thrown and process is started
+                    //IF NO ELECTRICITY DATA, TRY AGAIN
+                    catch (WebException)
                     {
-                        PriceText(elering.Time, elering.Price);     //Show aqcuired data from Elering
-                        WriteLine("Boinc is not crunching numbers.\n");
-
-                        //START EXTERNAL PROCESS
-                        if (elering.Price < userSpecifiedElectricityPrice)   //if price is below 45€/MWh and got data from elering, proceed to start process
-                        {
-                            boinc.Start();  //start process based on previous setup
-                            Thread.Sleep(13000);    //wait 13 seconds for program to connect to internet and get data from internet
-                            boinc.CloseMainWindow();    //close program window automatically
-
-                            WriteLine($"BOINC started crunching numbers!");
-                            WriteLine("Will check price again at next o'clock.\n");
-                            //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
-
-                            Thread.Sleep(10000);
-                        }
-                        //WAIT FOR SUITABLE MOMENT TO START PROCESS
-                        else
-                        {
-                            WriteLine($"Price is still too high for cruncing numbers!");
-                            WriteLine("Will check price again at next o'clock.\n");
-                            //Thread.Sleep(elering.Timestamp + 3600000);  //stop process for one hour inorder to check electricity price again one hour later
-
-                            Thread.Sleep(10000);
-                        }
+                        WriteLine("Could not get Elering data from internet.\n");
+                        retryCounter++;
+                        Thread.Sleep(5000);
                     }
                 }
-                //IF NO ELECTRICITY DATA, TRY AGAIN
-                catch (WebException)
+                catch (Exception)
                 {
-                    WriteLine("Could not get Elering data from internet.\n");
-                    retryCounter++;
-                    Thread.Sleep(5000);
+                    Clear();
+                    WriteLine("Please insert valid number.\n");
                 }
             }
         }
